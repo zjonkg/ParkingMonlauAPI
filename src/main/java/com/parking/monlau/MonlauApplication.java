e4291e470822579d6693f74fc198c990edb74fc8
Jon Goncalves <153616095+zjonkg@users.noreply.github.com>
2025-04-11 15:54:36 +0200

    Uploaded files

---
+++ src/main/java/com/parking/monlau/MonlauApplication.java
@@ -0,0 +1,13 @@
+package com.parking.monlau;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonlauApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonlauApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/parking/monlau/MonlauApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parking.monlau;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonlauApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
